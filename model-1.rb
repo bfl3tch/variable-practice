9880f018829ebfa1efa7c071a52a7e7a68ed3eda
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-03-31 12:18:49 -0400

    add last 5 game team results

--- model-1.rb
+++ model-1.rb
@@ -2,3 +2,7 @@ favorite_team = "tennessee titans"
 best_player = "derrick henry"
 henry_total_yards_2020 = 2141
 henry_average_yards_per_touch = 5.4
+nfl_leading_rusher = true
+titans_win_superbowl = false
+last_5_win_loss = ["W", "W", "L", "W", "L"]
+last_5_ppg = [31, 46, 40, 41, 20]
