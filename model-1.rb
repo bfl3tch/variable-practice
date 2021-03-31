033ec56dc65554fd0ba33d9987d825a70d277564
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-03-31 12:28:03 -0400

    add games played for 2 players, team record

--- model-1.rb
+++ model-1.rb
@@ -6,3 +6,5 @@ nfl_leading_rusher = true
 titans_win_superbowl = false
 last_5_win_loss = ["W", "W", "L", "W", "L"]
 last_5_ppg = [31, 46, 40, 41, 20]
+2020_games_played = { "derrick_henry" => 16, "adoree jackson" => 10 }
+2020_record = { "wins" => 11, "losses" => 5 }
